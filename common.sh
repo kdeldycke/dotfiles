c993e96eed8474c7091a2f7d14864961b23eda4f
Kevin Deldycke <kevin@deldycke.com>
2013-08-22 17:01:29 +0200

    Typo.

--- common.sh
+++ common.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-COMMON_PACKAGES="git vim jnettop hfsutils unrar subversion colordiff faac flac lame x264 inkscape graphviz qemu lftp shntool testdisk fdupesi recode pngcrush exiftooli rtmpdump optipng colortail colorsvn mercurial"
+COMMON_PACKAGES="git vim jnettop hfsutils unrar subversion colordiff faac flac lame x264 inkscape graphviz qemu lftp shntool testdisk fdupes recode pngcrush exiftooli rtmpdump optipng colortail colorsvn mercurial"
 
