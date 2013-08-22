25c34e5089d652bbcf1623b5a8b987791479a8d8
Kevin Deldycke <kevin@deldycke.com>
2013-08-22 18:15:01 +0200

    Typo.

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-COMMON_PACKAGES="git vim jnettop hfsutils unrar subversion colordiff faac flac lame x264 inkscape graphviz qemu lftp shntool testdisk fdupes recode pngcrush exiftooli rtmpdump optipng colortail colorsvn mercurial"
+COMMON_PACKAGES="git vim jnettop hfsutils unrar subversion colordiff faac flac lame x264 inkscape graphviz qemu lftp shntool testdisk fdupes recode pngcrush exiftool rtmpdump optipng colortail colorsvn mercurial"
 
 PYTHON_PACKAGES="pip setuptools virtualenv distribute pep8 pyflakes"
