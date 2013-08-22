6eafe0a64bd2ff98a27801e340d8b1c4f730edd4
Kevin Deldycke <kevin@deldycke.com>
2013-08-22 19:40:16 +0200

    Force dot files sync.

--- common.sh
+++ common.sh
@@ -3,3 +3,6 @@
 COMMON_PACKAGES="git vim jnettop hfsutils unrar subversion colordiff faac flac lame x264 inkscape graphviz qemu lftp shntool testdisk fdupes recode pngcrush exiftool rtmpdump optipng colortail colorsvn mercurial"
 
 PYTHON_PACKAGES="readline pip setuptools virtualenv distribute pep8 pyflakes"
+
+# Sync dot files
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".gitignore" --exclude ".gitmodules" --exclude "*.sh" --exclude "*.swp" --exclude "*.md" -av --no-perms . ~
