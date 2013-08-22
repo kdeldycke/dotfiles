0b3eb5c541ae1c23f43ac3ce771684f0984e051a
Kevin Deldycke <kevin@deldycke.com>
2013-08-22 18:33:51 +0200

    Force readline install for compatibility.

--- common.sh
+++ common.sh
@@ -2,4 +2,4 @@
 
 COMMON_PACKAGES="git vim jnettop hfsutils unrar subversion colordiff faac flac lame x264 inkscape graphviz qemu lftp shntool testdisk fdupes recode pngcrush exiftool rtmpdump optipng colortail colorsvn mercurial"
 
-PYTHON_PACKAGES="pip setuptools virtualenv distribute pep8 pyflakes"
+PYTHON_PACKAGES="readline pip setuptools virtualenv distribute pep8 pyflakes"
