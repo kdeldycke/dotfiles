8dc80a493011b7d41d81473211635c4927c8e5f4
Kevin Deldycke <kevin@deldycke.com>
2013-08-23 16:22:19 +0200

    Add some more Python packages.

--- common.sh
+++ common.sh
@@ -3,12 +3,15 @@
 # Ask for the administrator password upfront
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # We need to distinguish sources and binary packages for Brew & Cask on OSX
-COMMON_PACKAGES="git vim jnettop hfsutils unrar subversion colordiff faac flac lame x264 inkscape graphviz qemu lftp shntool testdisk fdupes recode pngcrush exiftool rtmpdump optipng colortail colorsvn mercurial"
+COMMON_PACKAGES="git vim jnettop hfsutils unrar subversion ack colordiff faac flac lame x264 inkscape graphviz qemu lftp shntool testdisk fdupes recode pngcrush exiftool rtmpdump optipng colortail colorsvn mercurial"
 BIN_PACKAGES="audacity avidemux dropbox firefox gimp inkscape vlc blender thunderbird virtualbox bitcoin-qt wireshark"
 
 # Define global Python packages
-PYTHON_PACKAGES="readline pip setuptools virtualenv distribute pep8 pyflakes"
+PYTHON_PACKAGES="readline pip setuptools distribute virtualenv virtualenvwrapper pep8 pylint pyflakes coverage"
 
 # Sync dot files
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude ".gitignore" --exclude ".gitmodules" --exclude "*.sh" --exclude "*.swp" --exclude "*.md" -av --no-perms . ~
