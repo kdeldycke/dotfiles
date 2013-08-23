b518324205f5d2ad2ee5ebc3d104b8787cc684a4
Kevin Deldycke <kevin@deldycke.com>
2013-08-23 10:53:27 +0200

    Ask for the administrator password upfront.

--- common.sh
+++ common.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
 COMMON_PACKAGES="git vim jnettop hfsutils unrar subversion colordiff faac flac lame x264 inkscape graphviz qemu lftp shntool testdisk fdupes recode pngcrush exiftool rtmpdump optipng colortail colorsvn mercurial"
 
 PYTHON_PACKAGES="readline pip setuptools virtualenv distribute pep8 pyflakes"
