789e7c02b3744a7b82740223c90031093f384abc
Kevin Deldycke <kevin@deldycke.com>
2013-09-10 14:11:47 +0200

    Rework package install order.

--- osx-install.sh
+++ osx-install.sh
@@ -17,6 +17,9 @@ ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 brew update
 brew upgrade
 
+# Include duplicates packages
+brew tap homebrew/dupes
+
 # Install Cask
 brew tap phinze/homebrew-cask
 brew install brew-cask
@@ -24,25 +27,21 @@ brew install brew-cask
 # Install OSX system requirements
 installcask x-quartz
 
+# Install a brand new Python
+brew install python
+brew link --overwrite python
+
 # Install common packages
 brew install $SOURCE_PACKAGES
 
-# Install vim with lua support
-brew install lua --completion
-brew install vim --with-lua --override-system-vim
+# Install OSX only packages
+brew install findutils bash ack grep rename tree webkit2png bazaar osxutils htop-osx p7zip faad2 bash-completion md5sha1sum
 
 # htop-osx requires root privileges to correctly display all running processes.
 sudo chown root:wheel /usr/local/bin/htop
 sudo chmod u+s /usr/local/bin/htop
 
-# Install OSX only packages
-brew install findutils bash ack rename tree webkit2png bazaar osxutils htop-osx p7zip faad2 bash-completion md5sha1sum
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
-
-# Install Python & co
-brew install python
-brew link --overwrite python
+# Install Python packages
 sudo pip install --upgrade $PYTHON_PACKAGES
 
 # Install binary apps
@@ -58,6 +57,10 @@ installcask iterm2
 installcask libre-office
 installcask tunnelblick
 # TODO: install Prey when https://github.com/phinze/homebrew-cask/pull/953 and https://github.com/phinze/homebrew-cask/pull/954 are addressed.
+
+# Install vim with lua support
+brew install lua --completion
+brew install vim --with-lua --override-system-vim
 
 # Clean things up
 brew doctor
