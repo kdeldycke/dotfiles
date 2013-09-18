0a8d71503ac35154155b21274bb7b5194f341d28
Kevin Deldycke <kevin@deldycke.com>
2013-09-19 00:46:34 +0200

    Remove double space.

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -18,7 +18,7 @@ chromium-browser bzr kompare avidemux-common transcode mkvtoolnix mencoder mplay
 p7zip-full htop gtk-chtheme gnome-themes-standard python-pip faad h264enc kwrite kscreensaver \
 hunspell-fr hunspell-dictionary-fr aspell-fr gimp-plugin-registry xscreensaver xscreensaver-data \
 xscreensaver-data-extra xscreensaver-gl xscreensaver-gl-extra network-manager-openvpn ksshaskpass \
-qemu-kvm dmg2img pdftk chromium-codecs-ffmpeg-extra picard xsltproc xfsprogs lm-sensors bzrtools  \
+qemu-kvm dmg2img pdftk chromium-codecs-ffmpeg-extra picard xsltproc xfsprogs lm-sensors bzrtools \
 ntp ca-certificates apt-file kdenlive python-dev gtk2-engines runsnakerun unclutter driftnet vim
 
 # Install global Python packages
