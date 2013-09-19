90a4c304001c504fa15a824eed95b66333d555f7
Kevin Deldycke <kevin@deldycke.com>
2013-09-19 16:45:37 +0200

    Remove empty line.

--- common.sh
+++ common.sh
@@ -2,7 +2,6 @@
 
 # Ask for the administrator password upfront
 sudo -v
-
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
