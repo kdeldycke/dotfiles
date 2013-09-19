f19a616b66a38e512b95436808d113575c0c9571
Kevin Deldycke <kevin@deldycke.com>
2013-09-19 14:10:47 +0200

    Typo.

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+# Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # We need to distinguish sources and binary packages for Brew & Cask on OSX
