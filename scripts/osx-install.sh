157f9b951ce2fcdd1ae480de2e8e08ff13a617e0
Kevin Deldycke <kevin@deldycke.com>
2013-12-08 18:56:47 +0100

    Install QuickLook plugins on OSX.

--- scripts/osx-install.sh
+++ scripts/osx-install.sh
@@ -89,6 +89,19 @@ brew cask install chromium
 brew cask install libre-office
 brew cask install tunnelblick
 
+# Install QuickLooks plugins
+# Source: https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install webp-quicklook
+brew cask install suspicious-package
+qlmanage -r
+
 # Install vim
 brew install lua --completion
 brew install cscope
