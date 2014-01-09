048574362217b2e4b35c1411190e72edb3a42191
Kevin Deldycke <kdeldycke@users.noreply.github.com>
2014-01-09 19:19:12 +0100

    Enable secure virtual memory

--- scripts/osx-config.sh
+++ scripts/osx-config.sh
@@ -414,6 +414,9 @@ defaults write com.apple.DiskUtility advanced-image-options -bool true
 # Disable the receiving of faxes.
 sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.efax.plist
 
+# Enable secure virtual memory
+sudo defaults write /Library/Preferences/com.apple.virtualMemory UseEncryptedSwap -bool yes
+
 # Remove default content
 rm -rf ~/Downloads/About\ Downloads.lpdf
 
