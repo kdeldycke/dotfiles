c153bc0749fb9c62fd7ac34c1e6759de8926f3e4
Kevin Deldycke <kevin@deldycke.com>
2014-04-19 23:16:06 +0200

    Use bash for consistency and keep environment variables.

--- scripts/ubuntu-install.sh
+++ scripts/ubuntu-install.sh
@@ -76,7 +76,7 @@ sudo aptitude install -y steam mesa-utils
 # Install xbox gamepad support
 # See: http://29a.ch/2013/2/24/xbox-controller-with-ubuntu-steam-xboxdrv
 sudo aptitude install -y xboxdrv
-sudo sh -c "echo 'blacklist xpad' > /etc/modprobe.d/blacklist.conf"
+sudo -E bash -c "echo 'blacklist xpad' > /etc/modprobe.d/blacklist.conf"
 sudo rmmod xpad
 
 
