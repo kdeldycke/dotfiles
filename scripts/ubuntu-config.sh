b955ae6579220008fb2bb0f93c3840658f8c3982
Kevin Deldycke <kdeldycke@users.noreply.github.com>
2014-10-27 20:34:30 +0100

    Always show grub boot menu.

--- scripts/ubuntu-config.sh
+++ scripts/ubuntu-config.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# Speed-up Grub boot
+# Speed-up Grub boot, but always show the boot menu.
 sudo sed -i 's/GRUB_TIMEOUT=10/GRUB_TIMEOUT=1/g' /etc/default/grub
+sudo sed -i 's/GRUB_HIDDEN_TIMEOUT/#GRUB_HIDDEN_TIMEOUT/g' /etc/default/grub
 sudo update-grub
 
 # Scan sensors
