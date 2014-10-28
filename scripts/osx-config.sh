5d8723463964d035577bc6d566e6bf98408deead
Kevin Deldycke <kdeldycke@users.noreply.github.com>
2014-10-28 12:27:19 +0100

    I don't use Transmission, Twitter or SizeUp app.

--- scripts/osx-config.sh
+++ scripts/osx-config.sh
@@ -446,7 +446,7 @@ done
 
 # Kill affected applications
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
-	"Dock" "Finder" "Mail" "Messages" "Safari" "SizeUp" "SystemUIServer" \
-	"Terminal" "Transmission" "Twitter" "iCal"; do
+	"Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer" \
+	"Terminal" "iCal"; do
 	killall "${app}" > /dev/null 2>&1
 done
