bef408f44dab4e54a2ba6d94542f9b4e276b8e83
Kevin Deldycke <kevin@deldycke.com>
2014-11-14 00:22:08 +0100

    Sort bin packages.

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ COMMON_PACKAGES="git git-extras legit jnettop hfsutils unrar subversion colordif
 lame x264 graphviz qemu lftp shntool testdisk fdupes recode pngcrush rtmpdump
 optipng colortail mercurial grc coreutils bzr htop apg fontforge"
 
-BIN_PACKAGES="audacity avidemux darktable firefox gimp hugin inkscape vlc blender thunderbird
-virtualbox wireshark prey inkscape"
+BIN_PACKAGES="audacity avidemux blender darktable firefox gimp hugin inkscape
+prey thunderbird virtualbox vlc wireshark"
 
 # Install all software first.
 if $IS_OSX; then
