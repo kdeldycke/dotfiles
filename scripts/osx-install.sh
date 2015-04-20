667d1d14d3b4773c83557fd4c9d778cbdc047aa1
Kevin Deldycke <kevin@deldycke.com>
2015-04-20 10:59:47 +0200

    Install spectacle for windows management.

--- scripts/osx-install.sh
+++ scripts/osx-install.sh
@@ -86,6 +86,7 @@ brew cask install --force flux
 brew cask install --force gitx
 brew cask install --force insync
 brew cask install --force libreoffice
+brew cask install --force spectacle
 brew cask install --force sqlite-database-browser
 brew cask install --force steam
 brew cask install --force torbrowser
