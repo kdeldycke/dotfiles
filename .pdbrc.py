1637c127919922d31f91dffc0bab77a545ade2f6
Kevin Deldycke <kevin@deldycke.com>
2015-04-23 16:23:59 +0200

    Persist PDB command history in home directory.

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,4 +1,31 @@
-# Autocomplete
+print "\n.pdbrc.py started"
+
+# Find home.
+import os
+home = os.curdir
+if 'HOME' in os.environ:
+    home = os.environ['HOME']
+elif os.name == 'posix':
+    home = os.path.expanduser("~/")
+# Make sure home always ends with a directory separator.
+home = os.path.realpath(home) + os.sep
+print "Home folder: " + home
+
+# Command line history.
+# Source: https://wiki.python.org/moin/PdbRcIdea
+import readline
+histfile = home + ".pdb_history"
+print "Command history: " + histfile
+try:
+    readline.read_history_file(histfile)
+except IOError:
+    pass
+import atexit
+atexit.register(readline.write_history_file, histfile)
+del histfile
+readline.set_history_length(20000)
+
+# Autocomplete.
 import rlcompleter
 pdb.Pdb.complete = rlcompleter.Completer(locals()).complete
 
@@ -10,3 +37,5 @@ termios_fd = sys.stdin.fileno()
 termios_echo = termios.tcgetattr(termios_fd)
 termios_echo[3] = termios_echo[3] | termios.ECHO
 termios_result = termios.tcsetattr(termios_fd, termios.TCSADRAIN, termios_echo)
+
+print ".pdbrc.py finished"
