9fd97647d8b25fe08fb733339f627fbcc6f3154a
Kevin Deldycke <kevin@deldycke.com>
2015-08-08 11:12:08 +0200

    Upgrade to refind 0.9.0.

--- scripts/osx-install.sh
+++ scripts/osx-install.sh
@@ -128,10 +128,10 @@ sudo rm -rf /Volumes/esp/EFI/refind
 sudo rm -rf /Volumes/esp/EFI/BOOT
 sudo diskutil umount /Volumes/esp
 # Install custom bootloader.
-curl -O http://garr.dl.sourceforge.net/project/refind/0.8.7/refind-bin-0.8.7.zip
-unzip ./refind-bin-0.8.7.zip
-./refind-bin-0.8.7/install.sh --alldrivers
-rm -rf ./refind-bin-0.8.7*
+curl -O http://garr.dl.sourceforge.net/project/refind/0.8.7/refind-bin-0.9.0.zip
+unzip ./refind-bin-0.9.0.zip
+./refind-bin-0.9.0/install.sh --alldrivers
+rm -rf ./refind-bin-0.9.0*
 sudo diskutil umount /Volumes/esp
 # Fix Yosemite boot. Source: http://www.rodsbooks.com/refind/yosemite.html
 mkdir /Volumes/esp
