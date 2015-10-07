kdeldycke/dotfiles

commit 841218fe35b2d240f334f3f2ab2c99b596402502

Privacy: disable Spotlight suggestions from Safari
Source: https://github.com/mathiasbynens/dotfiles/pull/455/files

scripts/osx-config.sh
@@ -5,7 +5,7 @@ sudo pmset -a womp 0
 # Automatic restart on power loss
 sudo pmset -a autorestart 1
 # Sets displaysleep to 10 minutes
-sudo pmset -a displaysleep 10
+sudo pmset -a displaysleep 10Privacy
 # Set standby delay to 3 hours (default is 1 hour)
 # See: http://www.ewal.net/2012/09/09/slow-wake-for-macbook-pro-retina/
 sudo pmset -a standbydelay 10800
@@ -303,6 +303,7 @@ defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool fals
 
 # Privacy: don’t send search queries to Apple
 defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
 
 # Show the full URL in the address bar (note: this still hides the scheme)
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true

commit 7e1ffa18141befe0013f09bd8c799825ef815fe3

Fix typo introduced in commit 841218fe.

scripts/osx-config.sh
@@ -11,7 +11,7 @@ sudo pmset -a womp 0
 # Automatic restart on power loss
 sudo pmset -a autorestart 1
 # Sets displaysleep to 10 minutes
-sudo pmset -a displaysleep 10Privacy
+sudo pmset -a displaysleep 10
 # Set standby delay to default 1 hour
 # See: http://www.ewal.net/2012/09/09/slow-wake-for-macbook-pro-retina/
 sudo pmset -a standbydelay 3600