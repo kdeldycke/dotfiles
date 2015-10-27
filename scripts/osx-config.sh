97b08279393a5ca850a21480525c7a9be8a3b181
Kevin Deldycke <kevin@deldycke.com>
2015-10-27 19:40:13 +0100

    Fix terminal theme path.

--- scripts/osx-config.sh
+++ scripts/osx-config.sh
@@ -598,7 +598,7 @@ tell application "Terminal"
 	(* Open the custom theme so that it gets added to the list
 	   of available terminal themes (note: this will open two
 	   additional terminal windows). *)
-	do shell script "open './assets" & themeName & ".terminal'"
+	do shell script "open './assets/" & themeName & ".terminal'"
 
 	(* Wait a little bit to ensure that the custom theme is added. *)
 	delay 1
