eaccf2f22bc4240b82267836c52a4b80b12ccf54
Kevin Deldycke <kevin@deldycke.com>
2015-10-29 00:31:27 +0100

    Use wily repository for insync.

--- scripts/kubuntu-install.sh
+++ scripts/kubuntu-install.sh
@@ -76,8 +76,8 @@ rm ./google-musicmanager-beta_current_amd64.deb
 
 # Install insync
 wget -qO - https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key | sudo apt-key add -
-sudo tee /etc/apt/sources.list.d/insync-vivid.list <<-EOF
-    deb http://apt.insynchq.com/ubuntu vivid non-free contrib
+sudo tee /etc/apt/sources.list.d/insync-wily.list <<-EOF
+    deb http://apt.insynchq.com/ubuntu wily non-free contrib
 EOF
 sudo aptitude update
 sudo aptitude install -y insync insync-dolphin
