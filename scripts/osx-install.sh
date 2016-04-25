30bd37c3ec127baa0a4ced526bcf670aafb7bb4c
Kevin Deldycke <kevin@deldycke.com>
2016-04-25 22:23:42 +0200

    Remove redundant quicklook plugins.
    See: https://github.com/sindresorhus/quick-look-plugins/issues/41

--- scripts/osx-install.sh
+++ scripts/osx-install.sh
@@ -94,14 +94,12 @@ brew cask install --force xbox360-controller-driver
 
 # Install QuickLooks plugins
 # Source: https://github.com/sindresorhus/quick-look-plugins
-brew cask install --force betterzipql
 brew cask install --force cert-quicklook
 brew cask install --force epubquicklook
 brew cask install --force qlcolorcode
 brew cask install --force qlmarkdown
 brew cask install --force qlprettypatch
 brew cask install --force qlstephen
-brew cask install --force quicklook-csv
 brew cask install --force quicklook-json
 brew cask install --force suspicious-package
 brew cask install --force webpquicklook
