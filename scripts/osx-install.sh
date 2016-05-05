3e76e1d689158dbf45a6322861390cf86e9999d1
Kevin Deldycke <kevin@deldycke.com>
2016-05-05 19:37:44 +0200

    Install a recent openssl.

--- scripts/osx-install.sh
+++ scripts/osx-install.sh
@@ -61,6 +61,7 @@ brew install exiftool
 brew install faad2
 brew install md5sha1sum
 brew install --with-qt5 mkvtoolnix
+brew install openssl
 brew install osxutils
 brew install pstree
 brew install ssh-copy-id
