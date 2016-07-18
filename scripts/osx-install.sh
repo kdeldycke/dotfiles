4b2f6216d7ce48043e134e7a6aa49530792bfd38
Kevin Deldycke <kdeldycke@scaleway.com>
2016-07-18 11:25:38 +0200

    Install qlvideo.

--- scripts/osx-install.sh
+++ scripts/osx-install.sh
@@ -114,6 +114,7 @@ brew cask install qlcolorcode
 brew cask install qlimagesize
 brew cask install qlmarkdown
 brew cask install qlstephen
+brew cask install qlvideo
 brew cask install quicklook-json
 brew cask install suspicious-package
 qlmanage -r
