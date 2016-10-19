7116df6bb75b961f3111ed7c934d9c18d307cda6
Kevin Deldycke <kevin@deldycke.com>
2016-10-19 16:44:31 +0200

    Reduce file distance.

--- scripts/macos-config.sh
+++ scripts/macos-config.sh
@@ -3,15 +3,15 @@ set -x
 
 # ~/.macos — https://mths.be/macos
 
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
 # Ask for the administrator password upfront
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Kill system preferences to not have it trying to override config we're about
-# to change.
-osascript -e "tell application \"System Preferences\" to quit"
 
 ###############################################################################
 # General UI/UX                                                               #
