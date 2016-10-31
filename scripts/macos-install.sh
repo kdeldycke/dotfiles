b10ca2f326cc85317b2267e1c62298ecc12a481d
Kevin Deldycke <kevin@deldycke.com>
2016-10-31 22:00:37 +0100

    Bump PCT version.

--- scripts/macos-install.sh
+++ scripts/macos-install.sh
@@ -207,7 +207,7 @@ duti -s io.mpv mp4
 
 # Install Popcorn Time.
 rm -rf /Applications/Popcorn-Time.app
-wget -O - "https://get.popcorntime.sh/repo/build/Popcorn-Time-0.3.9-Mac.tar.xz" | tar -xvJ --directory /Applications -f -
+wget -O - "https://get.popcorntime.sh/repo/build/Popcorn-Time-0.3.10-Mac.zip" | tar -xvz --directory /Applications -f -
 
 # Install and configure bitbar.
 brew cask install bitbar
