120e65cdc789528e3832e9dda700a6518e6458e0
Kevin Deldycke <kevin@deldycke.com>
2016-11-05 19:17:43 +0100

    Add some more Transmission tweaks.

--- scripts/macos-config.sh
+++ scripts/macos-config.sh
@@ -928,9 +928,15 @@ defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
 defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
 defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Documents/Torrents"
 
+# Use `~/Downloads` to store completed downloads
+defaults write org.m0k.transmission DownloadLocationConstant -bool true
+
 # Don’t prompt for confirmation before downloading
 defaults write org.m0k.transmission DownloadAsk -bool false
 defaults write org.m0k.transmission MagnetOpenAsk -bool false
+
+# Don’t prompt for confirmation before removing non-downloading active transfers
+defaults write org.m0k.transmission CheckRemoveDownloading -bool true
 
 # Trash original torrent files
 defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
@@ -945,6 +951,12 @@ defaults write org.m0k.transmission WarningLegal -bool false
 defaults write org.m0k.transmission BlocklistNew -bool true
 defaults write org.m0k.transmission BlocklistURL -string "http://john.bitsurge.net/public/biglist.p2p.gz"
 defaults write org.m0k.transmission BlocklistAutoUpdate -bool true
+
+# Randomize port on launch
+defaults write org.m0k.transmission RandomPort -bool true
+
+# Do not prevent computer from sleeping with active transfer
+defaults write org.m0k.transmission SleepPrevent -bool false
 
 ###############################################################################
 # Spectacle.app                                                               #
