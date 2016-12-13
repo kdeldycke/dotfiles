42db5f5a5e79bb9f3f2a2767333fecc3dad1d748
Kevin Deldycke <kevin@deldycke.com>
2016-12-13 11:21:34 +0100

    Asjust font smoothing and add reference.

--- scripts/macos-config.sh
+++ scripts/macos-config.sh
@@ -356,7 +356,8 @@ defaults write com.apple.screencapture type -string "png"
 defaults write com.apple.screencapture disable-shadow -bool true
 
 # Enable subpixel font rendering on non-Apple LCDs
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
+# Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
+defaults write NSGlobalDomain AppleFontSmoothing -int 1
 
 # Enable HiDPI display modes (requires restart)
 sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
