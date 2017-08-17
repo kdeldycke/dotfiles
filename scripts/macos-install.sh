24ec3334f5b837083e574dd3c25808a38042dfa3
Kevin Deldycke <kevin@deldycke.com>
2017-08-17 17:03:42 +0200

    Update bucket name.

--- scripts/macos-install.sh
+++ scripts/macos-install.sh
@@ -156,11 +156,11 @@ brew install homebrew/fuse/ntfs-3g
 # Install and configure Google Cloud Storage bucket mount point.
 brew install homebrew/fuse/gcsfuse
 mkdir -p "${HOME}/gcs"
-GOOGLE_APPLICATION_CREDENTIALS=~/.google-cloud-auth.json gcsfuse --implicit-dirs backup-imac-borg ./gcs
+GOOGLE_APPLICATION_CREDENTIALS=~/.google-cloud-auth.json gcsfuse --implicit-dirs backup-imac-restic ./gcs
 # Mount doesn't work as macOS doesn't let us register a new filesystem plugin.
 # See: https://github.com/GoogleCloudPlatform/gcsfuse/issues/188
 # sudo ln -s /usr/local/sbin/mount_gcsfuse /sbin/
-# mount -t gcsfuse -o rw,user,keyfile="${HOME}/.google-cloud-auth.json" backup-imac-borg "${HOME}/gcs"
+# mount -t gcsfuse -o rw,user,keyfile="${HOME}/.google-cloud-auth.json" backup-imac-restic "${HOME}/gcs"
 
 # Install restic for backups.
 brew tap restic/restic
