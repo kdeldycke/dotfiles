1d6cee0190f430cfac1e00edfd7c3619e9df7cd8
Kevin Deldycke <kevin@deldycke.com>
2017-08-18 10:38:25 +0200

    Limit Time Machine total backup size.

--- scripts/macos-config.sh
+++ scripts/macos-config.sh
@@ -862,6 +862,10 @@ defaults write com.apple.Terminal ShowLineMarks -int 0
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
+# Limit Time Machine total backup size to 1 TB (=1024*1024)
+# Source: http://www.defaults-write.com/time-machine-setup-a-size-limit-for-backup-volumes/
+sudo defaults write com.apple.TimeMachine MaxSize -integer 1048576
+
 # Disable local Time Machine backups
 hash tmutil &> /dev/null && sudo tmutil disablelocal
 
