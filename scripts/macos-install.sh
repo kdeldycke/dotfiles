85213d7935be93803dd3490356664dbece7d1fb4
Kevin Deldycke <kevin@deldycke.com>
2020-01-07 14:27:31 +0100

    Install Google Drive.

--- scripts/macos-install.sh
+++ scripts/macos-install.sh
@@ -83,6 +83,7 @@ brew cask install dupeguru
 brew cask install fork
 brew cask install ftdi-vcp-driver
 brew cask install gitup
+brew cask install google-drive-file-stream
 brew cask install iina
 brew cask install java
 brew cask install keybase
