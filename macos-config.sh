c59a4fd980efa0e645c608e7ba7dff4f4398e4cd
Kevin Deldycke <kevin@deldycke.com>
2020-01-26 18:45:19 +0100

    Add iChat section.

--- macos-config.sh
+++ macos-config.sh
@@ -1439,6 +1439,29 @@ defaults write com.apple.messageshelper.MessageController SOInputLineSettings -d
 # Disable continuous spell checking
 defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
 
+# Save history when conversations are closed
+defaults write com.apple.iChat SaveConversationsOnClose -bool true
+
+# Text size
+# 1: Small
+# 7: Large
+defaults write com.apple.iChat TextSize -int 2
+
+# Animate buddy pictures
+defaults write com.apple.iChat AnimateBuddyPictures -bool false
+
+# Play sound effects
+defaults write com.apple.messageshelper.AlertsController PlaySoundsKey -bool false
+
+# Notify me when my name is mentioned
+defaults write com.apple.messageshelper.AlertsController SOAlertsAddressMeKey -bool false
+
+# Notify me about messages form unknown contacts
+defaults write com.apple.messageshelper.AlertsController NotifyAboutKnockKnockKey -bool false
+
+# Show all buddy pictures in conversations
+defaults write com.apple.iChat ShowAllBuddyPictures -bool false
+
 ###############################################################################
 # Transmission.app                                                            #
 ###############################################################################
