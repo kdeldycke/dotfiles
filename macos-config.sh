91c746c4b9c38f99d5bf806aaf1d730fb1448e00
Kevin Deldycke <kevin@deldycke.com>
2020-01-26 18:43:00 +0100

    Setup terminal visual bells.

--- macos-config.sh
+++ macos-config.sh
@@ -1169,6 +1169,14 @@ defaults write com.apple.terminal SecureKeyboardEntry -bool true
 # Disable the annoying line marks
 defaults write com.apple.Terminal ShowLineMarks -int 0
 
+# Audible and Visual Bells
+/usr/libexec/PlistBuddy                                     \
+    -c "Delete :WindowSettings:Basic:Bell"                  \
+    -c "Add    :WindowSettings:Basic:Bell       bool false" \
+    -c "Delete :WindowSettings:Basic:VisualBell"            \
+    -c "Add    :WindowSettings:Basic:VisualBell bool true"  \
+    ~/Library/Preferences/com.apple.terminal.plist
+
 ###############################################################################
 # Time Machine                                                                #
 ###############################################################################
