f5e59f5d1e98bc5bc31fc9ca46919bf79b2919c7
Kevin Deldycke <kevin@deldycke.com>
2020-01-26 18:44:31 +0100

    Add calendar section.

--- macos-config.sh
+++ macos-config.sh
@@ -1311,6 +1311,51 @@ defaults write NSGlobalDomain NSPersonNameDefaultShouldPreferNicknamesPreference
 # Enable the debug menu in iCal (pre-10.8)
 defaults write com.apple.iCal IncludeDebugMenu -bool true
 
+# Days per week
+defaults write com.apple.iCal "n days of week" -int 7
+
+# Start week on:
+# 0: Sunday
+# 6: Saturday
+defaults write com.apple.iCal "first day of week" -int 1
+
+# Scroll in week view by:
+# 0: Day
+# 1: Week
+# 2: Week, Stop on Today
+defaults write com.apple.iCal "scroll by weeks in week view" -int 1
+
+# Day starts at:
+defaults write com.apple.iCal "first minute of work hours" -int 480
+
+# Day ends at:
+defaults write com.apple.iCal "last minute of work hours" -int 1080
+
+# Show X hours at a time
+defaults write com.apple.iCal "number of hours displayed" -int 16
+
+# Turn on timezone support
+defaults write com.apple.iCal "TimeZone support enabled" -bool true
+
+# Show events in year view
+defaults write com.apple.iCal "Show heat map in Year View" -bool true
+
+# Show week numbers
+defaults write com.apple.iCal "Show Week Numbers" -bool true
+
+# Open events in seperate windows
+# defaults write com.apple.iCal OpenEventsInWindowType -bool true
+
+# Ask before sending changes to events
+defaults write com.apple.iCal WarnBeforeSendingInvitations -bool true
+
+###############################################################################
+# Dashboard, TextEdit and Disk Utility                                        #
+###############################################################################
+
+# Enable Dashboard dev mode (allows keeping widgets on the desktop)
+defaults write com.apple.dashboard devmode -bool true
+
 # Use plain text mode for new TextEdit documents
 defaults write com.apple.TextEdit RichText -int 0
 # Open and save files as UTF-8 in TextEdit
