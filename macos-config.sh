3de99dab19e30ea32388bcdc5450a6a2c248f3d8
Kevin Deldycke <kevin@deldycke.com>
2020-06-21 13:10:36 +0200

    Add some plist resources.

--- macos-config.sh
+++ macos-config.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env zsh
+###############################################################################
+# Plist and preferences                                                       #
+###############################################################################
+# There is a couple of plist editing tools:
+#
+#  * defaults
+#    Triggers update notification update to running process, but usage is
+#    tedious.
+#
+#  * /usr/libexec/PlistBuddy
+#    Great for big update, can create non-existing files.
+#
+#  * plutil
+#    Can manipulate arrays and dictionaries with key paths.
+#
+# Sources:
+#   * https://scriptingosx.com/2016/11/editing-property-lists/
+#   * https://scriptingosx.com/2018/02/defaults-the-plist-killer/
+#   * https://apps.tempel.org/PrefsEditor/index.php
+
 set -x
 
 # Close any open System Preferences panes, to prevent them from overriding
