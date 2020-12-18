e346ef0539a11b1703581039fce6bfa2967b9357
Kevin Deldycke <kevin@deldycke.com>
2020-12-18 12:18:17 +0100

    Remove trailing spaces.

--- macos-config.sh
+++ macos-config.sh
@@ -518,10 +518,10 @@ sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutio
 
 # Only affects brightness, not contrast
 defaults write me.guillaumeb.MonitorControl lowerContrast -bool false
-defaults write me.guillaumeb.MonitorControl showContrast -bool false 
+defaults write me.guillaumeb.MonitorControl showContrast -bool false
 
 # Do not modify all screens at once
-defaults write me.guillaumeb.MonitorControl allScreens -bool false 
+defaults write me.guillaumeb.MonitorControl allScreens -bool false
 
 
 ###############################################################################
