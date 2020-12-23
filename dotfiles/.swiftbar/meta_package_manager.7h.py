74376fb3a3778168c43eeedf1a51410c5edcec5a
Kevin Deldycke <kevin@deldycke.com>
2020-12-23 17:32:09 +0100

    Fix mpm BitBar plugin symlink.

--- dotfiles/.swiftbar/meta_package_manager.7h.py
+++ dotfiles/.swiftbar/meta_package_manager.7h.py
@@ -1,1 +1,1 @@
-/usr/local/lib/python3.7/site-packages/meta_package_manager/bitbar/meta_package_manager.7h.py
+/usr/local/lib/python3.9/site-packages/meta_package_manager/bitbar/meta_package_manager.7h.py
