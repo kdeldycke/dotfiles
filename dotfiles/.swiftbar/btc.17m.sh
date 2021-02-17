61ad1b996b29f707a7337bbf7f601d3faadfde59
Wojtek Zubera <32733957+LuiGGi629@users.noreply.github.com>
2021-02-17 10:49:46 +0100

    Update btc.17m.sh

--- dotfiles/.swiftbar/btc.17m.sh
+++ dotfiles/.swiftbar/btc.17m.sh
@@ -12,4 +12,4 @@
 # by Damien Lajarretie
 # Based on Coinbase bitbar plugin by Mat Ryer
 
-echo -n "Ƀ$"; curl -s "https://www.bitstamp.net/api/ticker/" | egrep -o '"last": "[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/"last": //' | sed 's/\"//g'
+# echo -n "Ƀ$"; curl -s "https://www.bitstamp.net/api/ticker/" | egrep -o '"last": "[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/"last": //' | sed 's/\"//g'
