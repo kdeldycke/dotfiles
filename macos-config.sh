1b8e626758db167d85f28d2691ec4f8059cb7fc7
Kevin Deldycke <kevin@deldycke.com>
2022-01-02 22:20:21 +0100

    Use recent blocklist.

--- macos-config.sh
+++ macos-config.sh
@@ -2049,7 +2049,7 @@ defaults write org.m0k.transmission PlayDownloadSound -bool false
 # IP block list.
 # Source: https://giuliomac.wordpress.com/2014/02/19/best-blocklist-for-transmission/
 defaults write org.m0k.transmission BlocklistNew -bool true
-defaults write org.m0k.transmission BlocklistURL -string "http://john.bitsurge.net/public/biglist.p2p.gz"
+defaults write org.m0k.transmission BlocklistURL -string "https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz"
 defaults write org.m0k.transmission BlocklistAutoUpdate -bool true
 
 # Randomize port on launch
