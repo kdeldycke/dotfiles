0b49cc68f32b89b232d3a7f95894a196e51e26d6
Kevin Deldycke <kevin@deldycke.com>
2022-03-12 10:16:29 +0400

    Fix compatibility with BSD's sed.

--- install.sh
+++ install.sh
@@ -167,7 +167,7 @@ qlmanage -r cache
 XBAR_PLUGINS_FOLDER="${HOME}/Library/Application Support/xbar/plugins"
 mkdir -p "${XBAR_PLUGINS_FOLDER}"
 wget -O "${XBAR_PLUGINS_FOLDER}/btc.17m.sh" https://raw.githubusercontent.com/matryer/xbar-plugins/main/Cryptocurrency/Bitcoin/bitstamp.net/last.10s.sh
-sed -i "s/Bitstamp: /Ƀ/" "${XBAR_PLUGINS_FOLDER}/btc.17m.sh"
+sed -i "" "s/Bitstamp: /Ƀ/" "${XBAR_PLUGINS_FOLDER}/btc.17m.sh"
 wget -O "${XBAR_PLUGINS_FOLDER}/brew-services.7m.rb" https://raw.githubusercontent.com/matryer/xbar-plugins/main/Dev/Homebrew/brew-services.10m.rb
 chmod +x "${XBAR_PLUGINS_FOLDER}/"*.(sh|py|rb)
 open -a xbar
