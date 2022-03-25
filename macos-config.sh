863cb372f4ccf08974dee04ba4e631815a713327
Kevin Deldycke <kevin@deldycke.com>
2022-03-25 23:19:08 +0400

    Also hide read articles.

--- macos-config.sh
+++ macos-config.sh
@@ -2232,8 +2232,9 @@ defaults write com.ranchero.NetNewsWire-Evergreen SUHasLaunchedBefore -int 1
 # Refresh every 4 hours.
 defaults write com.ranchero.NetNewsWire-Evergreen refreshInterval -int 6
 
-# Hide read feeds.
+# Hide read feeds and articles.
 defaults write com.ranchero.NetNewsWire-Evergreen windowState -dict-add readFeedsFilterState -bool true
+defaults write com.ranchero.NetNewsWire-Evergreen windowState -dict-add readArticlesFilterStateValue -bool true
 
 
 ###############################################################################
